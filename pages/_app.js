6e691e3a348d0724922c1a8b68aafb85f35c3123
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-08-03 08:15:48 -0500

    Add material ui theme

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../theme/theme'
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
@@ -20,8 +21,10 @@ export default function MyApp(props) {
         <title>My page</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
-      <CssBaseline />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme} >
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </React.Fragment>
   );
 }
---
+++ theme/theme.js
@@ -0,0 +1,27 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+
+// Create a theme instance.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#9c19bd',
+    },
+    secondary: {
+      main: '#2683e0',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Montserrat'
+    ]
+  }
+});
+
+export default theme;

3834d4827a6d185c9abf1d4322d92a567a30b088
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-08-07 18:06:57 -0500

    Center the pricing tiers

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { Grid } from "@material-ui/core";
 export default function Home() {
   return (
     <div>
-      <Grid container style={{ margin: 0, width: '100%'}}spacing={5} alignItems="center" justify="center">
+      <Grid container style={{ margin: 0, width: '100%', height: '100vh'}}spacing={5} alignItems="center" justify="center">
         <Grid item>
           <Tier title="Free" featuresList={["Feature A", "Feature B"]}></Tier>
         </Grid>

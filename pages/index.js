f14d2b47e22c4895eb2c9454aa2fb8fc21ead16d
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-08-29 16:47:08 -0500

    Retrieve product tiers from stripe

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,38 @@
+import React, { useEffect, useState } from 'react';
 import { Tier } from "../components";
 import { Grid } from "@material-ui/core";
+import Stripe from 'stripe';
+
+Stripe = Stripe(process.env.NEXT_PUBLIC_STRIPE_API_KEY);
 
 export default function Home() {
+
+  const {products, setProducts} = useState({
+    products: [
+      {
+        price: 0,
+        priceId: ""
+      },
+      {
+        price: 0,
+        priceId: ""
+      }
+    ]
+  });
+  
+  useEffect( async () => {
+    const basicTierProduct = await Stripe.products.retrieve(
+      'prod_HvSNTZS6a19s8w'
+    );
+    const proTierProduct = await Stripe.products.retrieve(
+      'prod_HvSNTZS6a19s8w'
+    );
+    
+    setProducts({
+      products: [basicTierProduct, proTierProduct]
+    })
+  },[]);
+
   return (
     <div>
       <Grid container style={{ margin: 0, width: '100%', height: '100vh'}}spacing={5} alignItems="center" justify="center">

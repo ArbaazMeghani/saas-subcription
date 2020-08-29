50d18af6045fec2936b42d96d4bba528fed2a3b4
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-08-29 15:32:26 -0500

    Create subscription

--- firebase-cloud-functions/functions/index.js
+++ firebase-cloud-functions/functions/index.js
@@ -18,3 +18,30 @@ exports.createStripeCustomer = functions.https.onCall(async (user) => {
     setup_secret: intent.client_secret,
   });
 });
+
+exports.createSubscription = functions.https.onCall(async (customerId, paymentMethodId) => {
+  try {
+    await Stripe.paymentMethods.attach(paymentMethodId, {
+      customer: customerId,
+    });
+  } catch (error) {
+    return res.status('402').send({ error: { message: error.message } });
+  }
+
+  await Stripe.customers.update(
+    customerId, 
+    {
+      invoice_settings: {
+        default_payment_method: paymentMethodId,
+      },
+    }
+  );
+
+  const subscription = await Stripe.subscriptions.create({
+    customer: customerId,
+    items: [{ price: 'price_HGd7M3DV3IMXkC' }],
+    expand: ['latest_invoice.payment_intent'],
+  });
+
+  return JSON.stringify(subscription);
+});

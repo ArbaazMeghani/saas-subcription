0187a7ab38d90a6aaa747d89e45544e8b1d959ed
Arbaaz Meghani <arbaazmeghani@gmail.com>
2020-09-01 18:55:43 -0500

    center dashboard text

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 
 const dashoard = () => {
   return (
-    <div>
-      dashboard
-    </div>
+    <Grid container style={{ margin: 0, width: '100%', height: '100vh'}}spacing={5} alignItems="center" justify="center">
+      <Grid item>
+        <h1>
+          Dashboard
+        </h1>
+      </Grid>
+    </Grid>
   )
 }
 
